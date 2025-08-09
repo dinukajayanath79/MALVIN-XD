d13c7d13d999457c9df398365e247108041268fd
dinukajayanath79 <dinukajayanath79@gmail.com>
2025-08-09 17:10:02 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~Fv1RFLID#wkBH6GemvjDc3R6G0fnBhQchlVBWxnZT0YXdCx9dPUs",  // add your Session Id here
   
   OWNER_NUMBER: "263714757857", // put your phone number here
   
